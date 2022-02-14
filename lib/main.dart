5670599d38dc29ff2a7b30b8c19f2aa993b65128
lindreas <lindgren.andreas@gmail.com>
2022-02-14 22:49:04 +0200

    created transitions to quickgame and customgame pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,136 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:path/path.dart';
-import 'package:flutter/widgets.dart';
-import 'package:sqflite/sqflite.dart';
 import 'db.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(FlibersApp(new Logic()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp();
+class Logic {
+  //Change name of logic to maybe database value choices?
+  void quickGame() {
+    print("täällä 1");
+  }
+
+  void customGame() {
+    print("täällä 2");
+  }
+}
+
+class FlibersApp extends StatelessWidget {
+  final Logic logic;
+
+  FlibersApp(this.logic);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+      debugShowCheckedModeBanner: false,
+      home: new HomePage(logic),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final Logic logic;
+
+  HomePage(this.logic);
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 20, fontFamily: "Droid Sans"),
+        primary: const Color(0xffE47474),
+        minimumSize: const Size(225, 35));
+    return Scaffold(
+      backgroundColor: const Color(0xff194346),
+      body: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => QuickGame(logic)),
+              );
+            },
+            child: const Text('Quick Game'),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CustomGame(logic)),
+              );
+            },
+            child: const Text('Custom Game'),
+          ),
+        ],
+      )),
+    );
+  }
+
+  void _navigateAndDisplaySelection(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FlibersApp(logic)),
+    );
+
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text('$result')));
+  }
+}
+
+class QuickGame extends StatelessWidget {
+  final Logic logic;
+
+  QuickGame(this.logic);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //separate button for back function?
+        backgroundColor: const Color(0xff194346),
+      ),
+      backgroundColor: const Color(0xff194346),
+      body: const Center(
+        child: SizedBox(
+          width: 100.0,
+          height: 100.0,
+          child: Card(child: Text("Please select difficulty level")),
         ),
       ),
     );
   }
 }
 
-/*
-class MyHomePage extends StatelessWidget {
-  const MyHomePage()
-}*/
+class CustomGame extends StatelessWidget {
+  final Logic logic;
+
+  CustomGame(this.logic);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //separate button for back function?
+        backgroundColor: const Color(0xff194346),
+      ),
+      backgroundColor: const Color(0xff194346),
+      body: const Center(
+        child: SizedBox(
+          width: 100.0,
+          height: 100.0,
+          child: Card(child: Text("Please select difficulty level")),
+        ),
+      ),
+    );
+  }
+}
